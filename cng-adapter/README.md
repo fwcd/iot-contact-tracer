dc1030e275522f0343d709835eaef33b4afa6278
fwcd <fwcdmail@gmail.com>
2020-06-11 00:43:08 +0200

    WIP: Add adapter server

--- cng-adapter/README.md
+++ cng-adapter/README.md
@@ -1,2 +1,9 @@
 # Contiki NG Adapter
 A TCP server that accepts connections from Contiki-NG/Coojas serial port interface and mediates between the Contiki-NG client and the actual server.
+
+## System Dependencies
+* Python 3
+
+## Running
+* Use `python3 -m ct_server` to run the development server (inside the venv).
+* The server is now listening under `http://localhost:7533`
---
+++ cng-adapter/cng_adapter/__main__.py
@@ -0,0 +1,21 @@
+import argparse
+from socketserver import StreamRequestHandler, TCPServer
+
+class CNGAdapterTCPHandler(StreamRequestHandler):
+    def handle(self):
+        raw = self.rfile.readline().strip()
+        print(raw)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=7533, help="Sets the port of the server")
+
+    args = parser.parse_args()
+    port = args.port
+
+    with TCPServer(("localhost", port), CNGAdapterTCPHandler) as server:
+        print(f"Listening on port {port} for TCP connections...")
+        server.serve_forever()
+
+if __name__ == "__main__":
+    main()
