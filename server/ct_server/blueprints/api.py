5851ab527c6f06a887c66b36c2784590da06f209
fwcd <fwcdmail@gmail.com>
2020-06-14 00:33:51 +0200

    Setup endpoint for fetching all exposures

--- server/ct_server/blueprints/api.py
+++ server/ct_server/blueprints/api.py
@@ -1,12 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from ct_server.resources.exposures import Exposures
+from ct_server.resources.exposure import ExposureResource, ExposureListResource
 
 def create_blueprint():
     bp = Blueprint("api", __name__, url_prefix="/api/v1")
     api = Api(bp)
 
-    api.add_resource(Exposures, "/exposures/<string:id>")
+    api.add_resource(ExposureListResource, "/exposures")
+    api.add_resource(ExposureResource, "/exposures/<string:id>")
 
     return bp
--- server/ct_server/resources/exposure.py
+++ server/ct_server/resources/exposure.py
@@ -3,11 +3,11 @@ from flask_restful import marshal_with, fields, Resource
 from ct_server.models import Exposure, db
 
 exposure_marshaller = {
-    "id": fields.String,
-    "timestamp": fields.DateTime(dt_format="iso8601")
+    "id": fields.String
+    # "timestamp": fields.DateTime(dt_format="iso8601")
 }
 
-class Exposures(Resource):
+class ExposureResource(Resource):
     @marshal_with(exposure_marshaller)
     def get(self, id):
         return Exposure.query.filter_by(id=id).first_or_404()
@@ -17,3 +17,8 @@ class Exposures(Resource):
         db.session.add(exposure)
         db.session.commit()
         return "Created", 201, {}
+
+class ExposureListResource(Resource):
+    @marshal_with(exposure_marshaller)
+    def get(self):
+        return Exposure.query.all()
