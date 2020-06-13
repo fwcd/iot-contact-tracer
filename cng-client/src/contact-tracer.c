31289f1f8130865994a4c8ac488b9c95f18922fe
fwcd <fwcdmail@gmail.com>
2020-06-13 20:34:21 +0200

    Add basic rolling identifier structures

--- cng-client/src/contact-tracer.c
+++ cng-client/src/contact-tracer.c
@@ -1,3 +1,4 @@
+#include <random.h>
 #include <contiki.h>
 #include <dev/leds.h>
 #include <sys/node-id.h>
@@ -6,7 +7,80 @@
 #define LOG_MODULE "Contact tracer"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
+// The interval of seconds at which new identifiers are generated
+#define EXPIRATION_TIME (5 * 60)
+
+// The interval of seconds at which identifiers are broadcasted
+#define REBROADCAST_TIME 10
+
+// The number of seconds for which an identifier is stored
+#define INCUBATION_TIME (14 * 24 * 3600)
+
+// The number of stored identifiers for a single person
+#define STORED_IDENTIFIER_COUNT ((INCUBATION_TIME) / (EXPIRATION_TIME))
+
+// The (theoretical) number of other persons for which a full history could be maintained
+#define PERSON_COUNT 2
+
 // == Common ==
+
+#define ARRAY_SIZE(XS) (sizeof(XS) / sizeof(XS[0]))
+
+/** A rolling identifier to identify a per. */
+struct rolling_identifier {
+    uint16_t value[4];
+};
+
+uint8_t rolling_identifier_equals(struct rolling_identifier lhs, struct rolling_identifier rhs) {
+    uint8_t i;
+    for (i = 0; i < ARRAY_SIZE(lhs.value); i++) {
+        if (lhs.value[i] != rhs.value[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/** A structure holding the full history a person. */
+struct identifier_store {
+    struct rolling_identifier identifiers[STORED_IDENTIFIER_COUNT];
+    uint16_t size;
+};
+
+void identifier_store_insert(struct identifier_store *self, struct rolling_identifier identifier) {
+    if (self->size < ARRAY_SIZE(self->identifiers)) {
+        self->identifiers[self->size] = identifier;
+        self->size++;
+    } else {
+        LOG_WARN("Identifier store overflowing.\n");
+    }
+}
+
+uint8_t identifier_store_contains(struct identifier_store *self, struct rolling_identifier identifier) {
+    uint8_t i;
+    for (i = 0; i < self->size; i++) {
+        if (rolling_identifier_equals(self->identifiers[i], identifier)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/** A structure holding the full history of own and other identifiers. */
+struct known_identifiers {
+    struct identifier_store own;
+    struct identifier_store others[PERSON_COUNT];
+};
+
+/** Generates a new identifier (pseudo-)randomly. */
+struct rolling_identifier generate_identifier(void) {
+    struct rolling_identifier ident;
+    uint8_t i;
+    for (i = 0; i < ARRAY_SIZE(ident.value); i++) {
+        ident.value[i] = random_rand();
+    }
+    return ident;
+}
 
 // == Main ==
 
@@ -14,11 +88,16 @@ PROCESS(contact_tracer_process, "Contact tracer process");
 AUTOSTART_PROCESSES(&contact_tracer_process);
 
 PROCESS_THREAD(contact_tracer_process, env, data) {
-    // static struct etimer timer;
+    static struct etimer timer;
 
     PROCESS_BEGIN();
 
-    LOG_INFO("Test test 123\n");
+    etimer_set(&timer, REBROADCAST_TIME * CLOCK_SECOND);
+
+    while (true) {
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+        // TODO
+    }
 
     PROCESS_END();
 }
