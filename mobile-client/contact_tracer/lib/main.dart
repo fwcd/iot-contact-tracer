9094ce0581d9ced0b0d249af525a33dea29ad927
fwcd <fwcdmail@gmail.com>
2020-06-24 20:21:35 +0200

    Extract FeedCard and switch to 2-space-indent in app

--- mobile-client/contact_tracer/lib/main.dart
+++ mobile-client/contact_tracer/lib/main.dart
@@ -1,85 +1,83 @@
+import 'package:contact_tracer/feed_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(ContactTracerApp());
+  runApp(ContactTracerApp());
 }
 
 class ContactTracerApp extends StatelessWidget {
-    // The root of the application
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'IoT Contact Tracer',
-            theme: ThemeData(
-                // Primary application theme
-                primarySwatch: Colors.blueGrey,
-                // This makes the visual density adapt to the platform that you run
-                // the app on. For desktop platforms, the controls will be smaller and
-                // closer together (more dense) than on mobile platforms.
-                visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-            home: ContactTracerHomePage(title: 'IoT Contact Tracer'),
-        );
-    }
+  // The root of the application
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IoT Contact Tracer',
+      theme: ThemeData(
+        // Primary application theme
+        primarySwatch: Colors.blueGrey,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ContactTracerHomePage(title: 'IoT Contact Tracer'),
+    );
+  }
 }
 
 class ContactTracerHomePage extends StatefulWidget {
-    ContactTracerHomePage({Key key, this.title}) : super(key: key);
+  ContactTracerHomePage({Key key, this.title}) : super(key: key);
 
-    // Home page of the application.
-    // Fields are always final in a widget subclass.
+  // Home page of the application.
+  // Fields are always final in a widget subclass.
 
-    final String title;
+  final String title;
 
-    @override
-    _ContactTracerHomePageState createState() => _ContactTracerHomePageState();
+  @override
+  _ContactTracerHomePageState createState() => _ContactTracerHomePageState();
 }
 
 class _ContactTracerHomePageState extends State<ContactTracerHomePage> {
-    bool _healthy = false;
+  bool _healthy = false;
 
-    void _toggleExposed() {
-        setState(() {
-            _healthy = !_healthy;
-        });
-    }
+  void _toggleExposed() {
+    setState(() {
+      _healthy = !_healthy;
+    });
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Column(
-                children: <Widget>[
-                    Card(
-                        color: _healthy ? Colors.green[700] : Colors.red,
-                        child: SizedBox(
-                            width: double.infinity,
-                            child: Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Column(
-                                    children: <Widget>[
-                                        Text(
-                                            _healthy ? "Healthy" : "Exposed",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 25.0,
-                                                fontWeight: FontWeight.bold
-                                            )
-                                        ),
-                                    ],
-                                )
-                            )
-                        )
-                    )
-                ],
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _toggleExposed,
-                tooltip: 'Set Exposed',
-                child: Icon(Icons.error),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          FeedCard(
+            color: _healthy ? Colors.green[700] : Colors.red,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  _healthy ? 'Healthy' : 'Exposed',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold
+                  )
+                ),
+              ],
+            )
+          ),
+          FeedCard(
+            child: Text('Test')
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleExposed,
+        tooltip: 'Set Exposed',
+        child: Icon(Icons.error),
+      ),
+    );
+  }
 }
