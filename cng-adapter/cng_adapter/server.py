8805741d037aec9fefc071e6ff96eb0abddff016
fwcd <fwcdmail@gmail.com>
2020-06-25 17:36:37 +0200

    Multithread the CNG-Adapter

--- cng-adapter/cng_adapter/server.py
+++ cng-adapter/cng_adapter/server.py
@@ -1,7 +1,7 @@
 import socket
-from socketserver import TCPServer
+from socketserver import ThreadingMixIn, TCPServer
 
-class CNGAdapterTCPServer(TCPServer):
+class CNGAdapterTCPServer(ThreadingMixIn, TCPServer):
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(self.server_address)
