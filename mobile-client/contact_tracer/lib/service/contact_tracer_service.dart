83491d1a80cc14f2c204e3ed45aa21ec31721fdc
fwcd <fwcdmail@gmail.com>
2020-06-26 02:16:32 +0200

    Implement contact tracer starting

--- mobile-client/contact_tracer/lib/service/contact_tracer_service.dart
+++ mobile-client/contact_tracer/lib/service/contact_tracer_service.dart
@@ -2,21 +2,49 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:beacon_broadcast/beacon_broadcast.dart';
+import 'package:flutter_beacon/flutter_beacon.dart';
 
 const _broadcastInterval = Duration(seconds: 10);
+const _identifier = "iot-contact-tracer";
+const _magicUUIDPrefix = "d1e58b99-ddfb-4f13-a8db-aa401904"; // ...missing 2 bytes at the end
 
 class ContactTracerService {
+  final rx = flutterBeacon;
   final tx = BeaconBroadcast();
   final Stream<int> currentIdentifier = _startGeneratingIdentifiers();
 
+  bool running = false;
   List<StreamSubscription> _subscriptions = [];
 
-  ContactTracerService() {
+  Future<void> initialize() async {
+    await rx.initializeAndCheckScanning;
+
     // Magic UUID constant that is the same across all contact tracing nodes
-    tx.setUUID("d1e58b99-ddfb-4f13-a8db-aa4019043fd3");
+    tx.setIdentifier(_identifier); // TODO: iOS only, unfortunately
     currentIdentifier.listen((ident) { 
-      tx.setIdentifier(ident.toString()); // TODO: iOS only, unfortunately
+      tx.setUUID(_encodeAsUUID(ident));
     });
+  }
+
+  /// Starts the contact tracer and returns the stream of identifiers.
+  Future<Stream<int>> start() async {
+    if (running) {
+      return Stream.empty();
+    }
+    running = true;
+    await _startTx();
+    return _startRx();
+  }
+
+  /// Stops the contact tracer.
+  Future<void> stop() async {
+    if (!running) {
+      return;
+    }
+    await tx.stop();
+    _subscriptions.forEach((sub) { sub.cancel(); });
+    _subscriptions = [];
+    running = false;
   }
 
   static Stream<int> _startGeneratingIdentifiers() {
@@ -29,19 +57,25 @@ class ContactTracerService {
     );
   }
 
-  Future<void> _startRx() async {
-    // TODO
+  Stream<int> _startRx() {
+    return rx
+      .ranging(<Region>[Region(identifier: _identifier)])
+      .expand((event) {
+        // TODO: Filter based on proximity (above a certain threshold?)
+        return event.beacons
+          .map((b) => _decodeFromUUID(b.proximityUUID));
+      });
   }
 
   Future<void> _startTx() async {
-    // TODO
+    await tx.start();
   }
 
-  Future<void> start() async {
-    await Future.wait([_startRx(), _startTx()]);
+  static String _encodeAsUUID(int ident) {
+    return "$_magicUUIDPrefix${ident.toRadixString(16)}";
   }
 
-  Future<void> stop() async {
-    // TODO
+  static int _decodeFromUUID(String uuid) {
+    return int.parse(uuid.substring(uuid.length - 4), radix: 16);
   }
 }
